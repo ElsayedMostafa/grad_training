4aa7a49bf8afa40b70cd11ea14c4716f033eeb17
ElsayedMostafa <elsayedmostafa03@gmail.com>
2018-05-01 01:38:12 +0200

    edit font-x

--- Training/app/src/main/java/com/example/madara/training/webservices/Urls.java
+++ Training/app/src/main/java/com/example/madara/training/webservices/Urls.java
@@ -5,6 +5,6 @@ package com.example.madara.training.webservices;
  */
 
 public class Urls {
-    //public static final String MAIN_URL = "http://192.168.1.4/myproject/";
-    public static final String MAIN_URL = "http://192.168.43.191:8000/api/";
+    public static final String MAIN_URL = "http://192.168.1.5/myproject/";
+    //public static final String MAIN_URL = "http://192.168.43.191:8000/api/";
 }
